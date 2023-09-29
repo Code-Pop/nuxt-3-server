2044d24aa39dcaf983259ec4b9e9c50a9ff06471
Ben Hong <ben@bencodezen.io>
2023-09-29 09:08:06 -0700

    feat: add error handling

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <script setup>
-const { data: scizor } = await useFetch('/api/charizard')
+const { data: scizor, error } = await useFetch('/api/newmew')
 </script>
 
 <template>
   <h1>Home Page for Nuxt 3 Server Course</h1>
-  <ul>
+  <ul v-if="scizor">
     <li>
       <img :src="scizor.sprite" />
     </li>
     <li>{{ scizor.id }}: {{ scizor.name }}</li>
   </ul>
+  <div v-else>
+    <p>{{ error }}</p>
+  </div>
 </template>
--- server/api/[pokemon].ts
+++ server/api/[pokemon].ts
@@ -1,14 +1,23 @@
 export default defineEventHandler(async event => {
-  const { pokemon } = event.context.params
-  const response = await $fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  try {
+    const { pokemon } = event.context.params
+    const response = await $fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    )
 
-  console.log(event.context)
+    console.log(event.context)
 
-  const idealResponse = {
-    id: response.id,
-    name: response.name,
-    sprite: response.sprites.front_default
+    const idealResponse = {
+      id: response.id,
+      name: response.name,
+      sprite: response.sprites.front_default
+    }
+
+    return idealResponse
+  } catch (error) {
+    throw createError({
+      statusCode: 404,
+      message: "Can't find the Pokemon!"
+    })
   }
-
-  return idealResponse
 })
