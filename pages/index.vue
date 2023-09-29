d667de9bb5b689a45cd02fb530e81dc21c7403eb
Ben Hong <ben@bencodezen.io>
2023-09-29 08:39:18 -0700

    feat: add api call to pokeapi

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
+<script setup>
+const { data: scizor } = await useFetch(
+  'https://pokeapi.co/api/v2/pokemon/scizor'
+)
+</script>
+
 <template>
   <h1>Home Page for Nuxt 3 Server Course</h1>
+  <pre>
+    {{ scizor }}
+  </pre>
 </template>
